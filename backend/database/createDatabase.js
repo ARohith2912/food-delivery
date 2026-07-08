6e23676b8f9ce835df62b10df570185c1ec22fb4
Rohith <rohithambati5@gmail.com>
2026-07-08 15:36:15 +0530

    Update createDatabase for Aiven

--- backend/database/createDatabase.js
+++ backend/database/createDatabase.js
@@ -3,11 +3,15 @@ require("dotenv").config();
 
 async function createDatabase() {
     try {
-        const connection = await mysql.createConnection({
-            host: process.env.DB_HOST || "mysql-32c01c83-rohithambati5-578a.g.aivencloud.com",
-            user: process.env.DB_USER || "avnadmin",
-            password: process.env.DB_PASSWORD
-        });
+       const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
         await connection.query(
             `CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`
